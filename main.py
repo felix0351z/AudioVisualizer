a158839e5a9db35a3e5d20958fad756721cc492d
felix <felix0351z@gmail.com>
2022-10-20 19:33:19 +0200

    Added view.py and some changes

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ import sys
 import threading
 
 import numpy as np
-import pyqtgraph as pg
 import sacn
 from exp_filter import SimpleExpFilter
 import scipy.io.wavfile as wav
 from scipy.ndimage import gaussian_filter1d
-from PySide2 import QtWidgets, QtCore
+from PySide2 import QtCore
 from pydub import AudioSegment, playback
 
 import melbank as mel
 import processing as ps
+import view as view
 
 # Pfad der Audio-Datei
-INPUT_PAH = "/home/felix/Musik/10.wav"
+INPUT_PAH = "/home/felix/Musik/3.wav"
 
 FFT_BANDS = 1024
 MEL_BANDS = 30
@@ -26,8 +26,8 @@ WAVE_TIME = 50
 MIN_FREQ = 64
 MAX_FREQ = 12000
 
-ON_ESP = True
-ESP_ADRESS = "192.168.178.160"
+ON_ESP = False
+ESP_ADRESS = "192.168.178.169"
 
 # Verhältnis vom Abstand eines Frames zu seiner Länge
 B = 2.5
@@ -37,11 +37,16 @@ PLOT_OUTPUT = True
 # Werte über 0.1 sollen mehr geglättet werden als Werte hierunter
 mel_gain = SimpleExpFilter(np.tile(0.01, MEL_BANDS), alpha_decay=0.01, alpha_rise=0.99)
 mel_smoothing = SimpleExpFilter(np.tile(0.01, MEL_BANDS), alpha_decay=0.5, alpha_rise=0.99)
+gain = SimpleExpFilter(np.tile(0.01, MEL_BANDS), alpha_decay=0.001, alpha_rise=0.99)
 
 r_filt = SimpleExpFilter(np.tile(0.01, N_PIXELS // 2), alpha_decay=0.2, alpha_rise=0.99)
 b_filt = SimpleExpFilter(np.tile(0.01, N_PIXELS // 2), alpha_decay=0.1, alpha_rise=0.5)
 
+p_filt = SimpleExpFilter(np.tile(1, (3, N_PIXELS // 2)), alpha_decay=0.1, alpha_rise=0.99)
+
 common_mode = SimpleExpFilter(np.tile(0.01, N_PIXELS // 2), alpha_decay=0.99, alpha_rise=0.01)
+
+p = np.tile(1.0, (3, N_PIXELS // 2))
 
 
 class Program:
@@ -51,10 +56,8 @@ class Program:
     """
 
     def __init__(self):
-        # Erstellen der Haupt-Applikation und dem GraphFenster
-        self.app = QtWidgets.QApplication(sys.argv)
-        self.win = pg.GraphicsLayoutWidget(title="Audio Visualizer", size=(1920, 1080))
-        self.win.setAntialiasing(True)
+        # Erstellen der Applikation
+        self.app = view.Window("Audio Visualizer")
 
         # Laden der Wav-Audiodatei
         self.sample_rate, data = wav.read(INPUT_PAH)
@@ -88,29 +91,30 @@ class Program:
         basierend auf der SampleRate. Alle Taktstöße innerhalb einer Sekunde
         :return: WavePlot, xAxis
         """
-        wave_Canvas: pg.PlotItem = self.win.addPlot(title="Waveform", row=0, col=0)
-        wave_Canvas.setXRange(0, self.sample_rate)  # Immer eine Sekunde
-        wave_Canvas.setYRange(-0.5, 0.5)  # Ungefähre Amplituden Höchstwerte
+
+        x_range = (0, self.sample_rate)  # Immer eine Sekunde
+        y_range = (-0.5, 0.5)  # Ungefähre Amplituden Höchstwerte
+        plot = self.app.create_plot_item("Waveform", x_range, y_range)
 
         # Liste von 1 bis SampleRate für WavePlot
         x = np.arange(1, self.sample_rate + 1)
 
-        return wave_Canvas.plot(), x
+        return plot, x
 
     def plot_spectrum(self):
         """
         Spektrum, welches durch die Fourier-Transformation-Erzeugt wurde.
         :return: SpectrumPlot. xAxis
         """
-        spectrum_Canvas: pg.PlotItem = self.win.addPlot(title="Spectrum", row=1, col=0)
-        spectrum_Canvas.setYRange(0, 1)  # Maximale höher der Lautstärke
-        spectrum_Canvas.setXRange(1, 10 ** 5)  # Logarithmisch
-        spectrum_Canvas.setLogMode(x=True, y=False)  # dB später manuell berechnen
+
+        x_range = (1, 10 ** 5)  # Logarithmisch
+        y_range = (0, 1)
+        plot = self.app.create_plot_item("FFT", x_range, y_range, log=True)
 
         # Liste mit N/2 Einträgen. Von 0Hz bis 20.000Hz (Max für Menschen)
         x = np.linspace(0, 20000, int(FFT_BANDS / 2))
 
-        return spectrum_Canvas.plot(), x
+        return plot, x
 
     def plot_mel_spectrum(self):
         """
@@ -118,14 +122,15 @@ class Program:
         angepasst ist.
         :return: Mel bank-Spectrum Plot, xAxis
         """
-        melCanvas: pg.PlotItem = self.win.addPlot(title="Melbars Sectrum", row=0, col=1)
-        melCanvas.setYRange(0, 1)
-        melCanvas.setXRange(0, mel.hertz_to_mel(20000))  # 40 Peaks werden genutzt
+
+        x_range = (0, mel.hertz_to_mel(20000))  # 40 Peaks werden genutzt
+        y_range = (0, 1)
+        plot = self.app.create_plot_item("Mel-bar", x_range, y_range)
 
         # Liste mit N_MEL Einträgen. Von 0 Hz bis 20000Hz skaliert in Mel
         x = np.linspace(0, mel.hertz_to_mel(20000), MEL_BANDS)
 
-        return melCanvas.plot(), x
+        return plot, x
 
     def plot_led_output(self):
         """
@@ -133,20 +138,14 @@ class Program:
         :return: LED Plot, xAxis
         """
 
-        ledCanvas: pg.PlotItem = self.win.addPlot(title="LED Output", row=1, col=1, colspan=3)
-        ledCanvas.setYRange(0, 1)
-        ledCanvas.setXRange(0, N_PIXELS)
+        x_range = (0, N_PIXELS)
+        y_range = (0, 1)
 
-        # Unterschiedliche Farben jay
-        r_pen = pg.mkPen((255, 30, 30, 200), width=4)
-        g_pen = pg.mkPen((30, 255, 30, 200), width=4)
-        b_pen = pg.mkPen((30, 30, 255, 200), width=4)
-        r_curve = pg.PlotCurveItem(pen=r_pen)
-        g_curve = pg.PlotCurveItem(pen=g_pen)
-        b_curve = pg.PlotCurveItem(pen=b_pen)
-        ledCanvas.addItem(r_curve)
-        ledCanvas.addItem(g_curve)
-        ledCanvas.addItem(b_curve)
+        r_pen = self.app.create_pen((255, 30, 30, 200), width=4)
+        g_pen = self.app.create_pen((30, 255, 30, 200), width=4)
+        b_pen = self.app.create_pen((30, 30, 255, 200), width=4)
+
+        r_curve, g_curve, b_curve = self.app.create_plot_curve_item("Led Output", x_range, y_range, pens=[r_pen, g_pen, b_pen])
 
         x = np.arange(1, N_PIXELS + 1)
 
@@ -253,8 +252,26 @@ class Program:
         max = np.max(gaussian_filter1d(mel_spectrum, sigma=1.0))
         mel_gain.update(max)
         mel_spectrum /= mel_gain.forcast
+
         # Mel Smoothing
         mel_spectrum = mel_smoothing.update(mel_spectrum)
+
+        # TODO bis 512 auffüllen!
+        # self.visualize_energy(mel_spectrum)
+        compressed: np.ndarray = self.visualize_spectrum(mel_spectrum)  # Effekt
+        out = compressed.transpose().flatten()
+
+        # Bei Erweiterungen muss auch geschaut werden, ob mehr als 512 in out sind!!!
+        final = np.clip(np.append(out, np.zeros(512 - len(out))), 0, 255)
+
+        tup = tuple([x.item() for x in np.around(final).astype(int)])
+
+        if ON_ESP:
+            self.sender[1].dmx_data = tup
+
+        self.current_frame += 1
+
+    def visualize_spectrum(self, mel_spectrum: np.ndarray) -> np.ndarray:
 
         # Logarithmische Darstellung
         mellog = np.log(mel_spectrum + 1)
@@ -279,20 +296,47 @@ class Program:
         self.g.setData(x=self.x_led, y=out_g)
         self.b.setData(x=self.x_led, y=out_b)
 
-        # TODO bis 512 auffüllen!
-        compressed: np.ndarray = np.array([out_r, out_g, out_b]) * 255
-        out = compressed.transpose().flatten()
-
-        # Bei Erweiterungen muss auch geschaut werden, ob mehr als 512 in out sind!!!
-        final = np.clip(np.append(out, np.zeros(512 - len(out))), 0, 255)
-
-        tup = tuple([x.item() for x in np.around(final).astype(int)])
-
-        if ON_ESP:
-            self.sender[1].dmx_data = tup
-
         self.prev_output = np.copy(output)
-        self.current_frame += 1
+
+        return np.array([out_r, out_g, out_b]) * 255
+
+    def visualize_energy(self, f: np.ndarray) -> np.ndarray:
+        global p
+
+        # Gain Normalization
+        f = np.copy(f)
+        gain.update(f)
+        f /= gain.forcast
+
+        # Scale by the width of the LEDs
+        f *= float((N_PIXELS // 2) - 1)
+        scale = 0.9
+
+        r = int(np.mean(f[:(len(f) // 3)] ** scale))  # Low for R
+        g = int(np.mean(f[(len(f) // 3):(2 * len(f) // 3)] ** scale))  # Middle for G
+        b = int(np.mean(f[2 * len(f) // 3:] ** scale))  # High for B
+
+        p[0, :r] = 255.0
+        p[0, r:] = 0.0
+        p[1, :g] = 255.0
+        p[1, g:] = 0.0
+        p[2, :b] = 255.0
+        p[2, b:] = 0.0
+
+        p_filt.update(p)
+        p = np.round(p_filt.forcast)
+
+        p[0, :] = gaussian_filter1d(p[0, :], sigma=4.0)
+        p[1, :] = gaussian_filter1d(p[1, :], sigma=4.0)
+        p[2, :] = gaussian_filter1d(p[2, :], sigma=4.0)
+
+        out_r = np.append(np.flip(p[0, :]), p[0, :])
+        out_g = np.append(np.flip(p[1, :]), p[1, :])
+        out_b = np.append(np.flip(p[2, :]), p[2, :])
+
+        self.r.setData(x=self.x_led, y=out_r)
+        self.g.setData(x=self.x_led, y=out_g)
+        self.b.setData(x=self.x_led, y=out_b)
 
     def start(self):
         """
@@ -311,17 +355,13 @@ class Program:
             if ON_ESP:
                 self.sender.start()
                 self.sender.activate_output(1)
-                self.sender[1].multicast = True
+                self.sender[1].multicast = False
                 self.sender[1].destination = ESP_ADRESS
         else:
             timer.start(WAVE_TIME)  # 20 FPS für Waveform + Spectrum
 
         self.playback_task.start()
-        self.run()
-
-    def run(self):
-        self.win.show()
-        self.app.exec_()
+        self.app.start()
 
 
 if __name__ == '__main__':
