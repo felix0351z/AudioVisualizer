549185c25d638ae10dd9edfd925a9d5dc61c5109
Felix Zimmermann <felix0351z@gmail.com>
2023-02-27 22:20:46 +0100

    Änderung der Bingröße auf die Länge des Fensters

--- tests/fensterung.py
+++ tests/fensterung.py
@@ -1,12 +1,10 @@
 import numpy as np
-import core.view as view
-from dsp import filter, processing
+import src.core.view as view
+from src.dsp import filter
 import consts
 
 N_FFT = 1024
 pre_emphasis = filter.SimplePreEmphasis()
-
-x = np.linspace(0, 20000, int(N_FFT / 2))
 
 
 class Window_Test:
@@ -18,7 +16,7 @@ class Window_Test:
         x_range = (1, 10 ** 5)  # Logarithmisch
         y_range = (0, 1)
         self.plot_none = window.create_plot_item("Kein Fenster", x_range, y_range, log=True)
-        self.plot_hamming = window.create_plot_item("Hamming Fenster", x_range, y_range, log=True)
+        self.plot_hamming = window.create_plot_item("Hanning Fenster", x_range, y_range, log=True)
 
         window.start(self.test)
 
@@ -27,13 +25,15 @@ class Window_Test:
 
         f = np.append(self.last_frame, signal)
 
-        hamming = f * np.hanning(len(f))
-        none_fft = processing.rfft(signal, n=N_FFT)**2
-        hamming_fft = processing.rfft(hamming, n=N_FFT)**2
+        hanning = f * np.hanning(len(f))
+        none_fft = np.abs(np.fft.rfft(signal)) ** 2
+        hanning_fft = np.abs(np.fft.rfft(hanning)) ** 2
 
-        self.plot_none.setData(x=x, y=none_fft)
-        self.plot_hamming.setData(x=x, y=hamming_fft)
+        x = lambda n: np.linspace(0, 20000, len(n))
+        self.plot_none.setData(x=x(none_fft), y=none_fft)
+        self.plot_hamming.setData(x=x(hanning_fft), y=hanning_fft)
 
         self.last_frame = signal
+
 
 Window_Test()
