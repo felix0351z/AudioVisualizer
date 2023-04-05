3df84dec0d533d3d091e1c9c72e80d0129a42be2
Felix Zimmermann <felix0351z@gmail.com>
2023-04-05 15:21:12 +0200

    Added smoothing and logarithmic test

---
+++ tests/smoothing.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+import utils
+from src.dsp import exponential_smoothing
+from src.dsp import filter
+from src.utils import view
+
+# This test is based on the gain normalization test and goes a step further and tries to apply a smoothing transition over time
+
+mel_bins = 60  # Number of mel bins
+minimum_frequency = 20  # Minimum frequency, which will be measured by the filterbank
+maximum_frequency = 8000  # Maximum frequency, which will be measured by the filterbank
+
+pre_emphasis_filter = filter.SimplePreEmphasis()  # Reduce the amount low frequencies for a better representation
+x_axis = np.linspace(1, mel_bins + 1, mel_bins)
+
+gain_normalization_filter = exponential_smoothing.SingleExponentialFilter(start_value=0.1, alpha_rise=0.99,
+                                                                          alpha_decay=0.1)
+
+# If the smoothing factor of the decay is lower, the decay will need more time to come back to the bottom.
+# The result should be a better transition over time.
+mel_smoothing = exponential_smoothing.DimensionalExponentialFilter(start_value=np.tile(0.1, mel_bins), alpha_rise=0.99,
+                                                                   alpha_decay=0.2)
+
+
+class Smoothing:
+
+    def __init__(self):
+        window = view.Window("Smoothing Test")
+
+        x_range = (1, 60)
+        y_range = (0, 1)
+
+        self.mel_plot = window.create_plot_item("Melbank", x_range, y_range, log=True)
+
+        self.smoothed1 = window.create_pen(color=(255, 0, 0, 200), width=1.5)
+        self.smoothed2 = window.create_pen(color=(0, 255, 0, 200), width=1.5)
+        self.s_plots = window.create_plot_curve_item("Smoothed Signal", x_range, y_range,
+                                                     pens=[self.smoothed1, self.smoothed2],
+                                                     log=False)
+        self.sender = utils.TestSender()
+
+        window.start(self.run)
+
+    def run(self, raw: np.ndarray):
+        mel_spectrum = utils.raw_to_mel_signal(mel_bins, raw, minimum_frequency, maximum_frequency)
+        max = np.max(mel_spectrum)
+
+        gain_normalization_filter.update(max)
+        normalized = mel_spectrum / gain_normalization_filter.forcast  # Normalize gain
+
+        smoothed = mel_smoothing.update(normalized)  # Take the forcast of the smoothing filter as actual value
+
+        self.mel_plot.setData(x=x_axis, y=normalized)
+        self.s_plots[0].setData(x=x_axis, y=smoothed)
+        # self.s_plots[1].setData(x=x_axis, y=?) Another graph for comparison
+        self.sender.send_signal(smoothed, color=(255, 255, 255))
+
+
+Smoothing()
