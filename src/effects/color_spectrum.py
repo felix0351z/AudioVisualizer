d7c05fa7e0488a282f4957c7ea392cab65a2ad47
Julian1408 <93348523+Julian1408@users.noreply.github.com>
2023-05-21 18:02:35 +0200

    Added color_spectrum effect

---
+++ src/effects/color_spectrum.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+from src.dsp.melbank import Melbank
+from src.effects.spectrum import SpectrumEffect
+from src.effects.effect import ColorRender
+from src.colors.effect import pack_color_with_signal
+
+
+class ColorSpectrumEffect(SpectrumEffect, ColorRender):
+    NAME = "Color Spectrum"
+    DESCRIPTION = "Separates the current spectrum in three parts (low, middle and high frequencies) and displays each with a different color"
+
+    COLOR_LOW = (255, 0, 0)
+    COLOR_MIDDLE = (0, 255, 0)
+    COLOR_HIGH = (0, 0, 255)
+
+    def start(self):
+        self.melbank = Melbank(
+            bins=super().amount_leds(),
+            sample_rate=super().sample_rate(),
+            gain=self.GAIN,
+            smoothing=self.SMOOTHING
+        )
+
+    def update(self, config):
+        pass
+
+    def get_color_spectrum(self, melbank):
+        low, middle, high = np.split(melbank, 3)
+
+        low = np.concatenate((low, low, low), axis=None)
+        middle = np.concatenate((middle, middle, middle), axis=None)
+        high = np.concatenate((high, high, high), axis=None)
+
+        low_flipped = pack_color_with_signal(np.append(np.flip(low), low), self.COLOR_LOW)
+        middle_flipped = pack_color_with_signal(np.append(np.flip(middle), middle), self.COLOR_MIDDLE)
+        high_flipped = pack_color_with_signal(np.append(np.flip(high), high), self.COLOR_HIGH)
+
+        stack = np.stack((low_flipped, middle_flipped))
+        output = np.max(stack, axis=0)
+
+        stack = np.stack((output, high_flipped))
+        output = np.max(stack, axis=0)
+
+        return output
+
+    def visualize_rgb(self) -> np.ndarray:
+        power_frame = super().power_spectrum()
+        melbank = self.melbank.get_melbank_from_signal(power_frame)
+
+        effect = self.get_color_spectrum(melbank)
+        return effect
